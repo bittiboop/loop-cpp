657f67f2b6b4f0ac47894418554f2cbdb877a0b4
bittiboop <sstrelkova15@gmail.com>
2024-04-29 20:39:12 +0300

    added some start code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <windows.h>
 using namespace std;
 
 int main() {
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	SetConsoleTextAttribute(hConsole, 7);
+	int st_range, en_range, max_dig;
+	cout << "Enter the start of range limit:";
+	cin >> st_range;
+	cout << "\n" << "Enter the end of range limit:";
+	cin >> en_range;
+	max_dig = st_range > en_range ? en_range : st_range;
+
+	do {
+		st_range++;
+		cout << st_range << endl;
+	} while (st_range < max_dig);
+	
+	
 
 	return 0;
 }
